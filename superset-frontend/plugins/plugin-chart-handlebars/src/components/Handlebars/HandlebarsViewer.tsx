39206a70842cb5fe306057f6bcdbf658e605b5f8
Jacob Ray Gilman <jacob@oscillas.com>
2025-04-10 13:42:14 -0500

    Install and register handlebars-group-by with handlebars plugin

--- superset-frontend/plugins/plugin-chart-handlebars/src/components/Handlebars/HandlebarsViewer.tsx
+++ superset-frontend/plugins/plugin-chart-handlebars/src/components/Handlebars/HandlebarsViewer.tsx
@@ -22,6 +22,7 @@ import moment from 'moment';
 import { useMemo, useState } from 'react';
 import { isPlainObject } from 'lodash';
 import Helpers from 'just-handlebars-helpers';
+import HandlebarsGroupBy from 'handlebars-group-by';
 
 export interface HandlebarsViewerProps {
   templateSource: string;
@@ -89,3 +90,4 @@ Handlebars.registerHelper('stringify', (obj: any, obj2: any) => {
 });
 
 Helpers.registerHelpers(Handlebars);
+HandlebarsGroupBy.register(Handlebars);
